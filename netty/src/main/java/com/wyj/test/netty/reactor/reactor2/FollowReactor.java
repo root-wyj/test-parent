4034e78a09b5074a5087f0c6e36fc9f4e7f99f63
wuyingjie <wuyingjie@alipaycampus.com>
2019-11-20 15:28:17 +0800

    test nio

--- netty/src/main/java/com/wyj/test/netty/reactor/reactor2/FollowReactor.java
+++ netty/src/main/java/com/wyj/test/netty/reactor/reactor2/FollowReactor.java
@@ -43,15 +43,15 @@ public class FollowReactor{
 
     public void register(SocketChannel channel) {
         try {
-            notifyExecutor.submit(() -> {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                selector.wakeup();
-                System.out.println("wake up");
-            });
+//            notifyExecutor.submit(() -> {
+//                try {
+//                    Thread.sleep(500);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                selector.wakeup();
+//                System.out.println("wake up");
+//            });
             channel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_CONNECT);
             System.out.println("register ed");
         } catch (ClosedChannelException e) {
@@ -59,13 +59,22 @@ public class FollowReactor{
         }
     }
 
+    /*
+    1. 先创建selector，然后启动线程 调用selector.select 方法. select默认会阻塞直到 有感兴趣的事件到来
+    2. 主reactor 监听 accept 事件，监听到了之后 将socketChannel 注册到 从reactor上，也就是 上面的selector上。
+    3. 这时候 会发现，socketChannel.register(selector, ints) 会阻塞，register 之后的代码 根本不会执行
+       这时候 会发现，client 发消息，selector 并不能成功监听到 read 事件
+       而且尝试在register 之前 启动线程 延迟调用 selector.wakeup 也不行。。。
+    4. 最后，只好使用 select(timeout) 的方式 来跑了。。。。
+       暂时没有找到更好的方式。。。
+     */
     private void start() {
         try {
-//            while (true) {
-                while (selector.select() > 0) {
-//                if (selector.select(500) <= 0) {
-//                    continue;
-//                }
+            while (true) {
+//                while (selector.select() > 0) {
+                if (selector.select(500) <= 0) {
+                    continue;
+                }
 
                 Set<SelectionKey> selectionKeys = selector.selectedKeys();
                 Iterator<SelectionKey> iterator = selectionKeys.iterator();
